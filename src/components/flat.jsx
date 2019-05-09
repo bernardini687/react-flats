245e32d4f9d3f2c2283bc38dc63821364bbb13ba
Oscar Bernardini <bernardini687@gmail.com>
2019-05-09 16:38:03 +0200

    Remove a note

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -20,8 +20,3 @@ const Flat = ({ info }) => {
 };
 
 export default Flat;
-
-/*
-style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),
-url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);"
-*/
